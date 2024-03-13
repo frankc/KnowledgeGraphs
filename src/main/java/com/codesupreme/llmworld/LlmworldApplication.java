45a82dd67d8ce3b753c54715925a2b3a50e20161
david <davidkrad17@gmail.com>
2024-03-12 21:03:56 -0500

    spring boot + jena framework

---
+++ src/main/java/com/codesupreme/llmworld/LlmworldApplication.java
@@ -0,0 +1,13 @@
+package com.codesupreme.llmworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmworldApplication.class, args);
+	}
+
+}
